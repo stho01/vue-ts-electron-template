d89a672e9b98248dd72b848d57743744d6fbbe08
Sten Marius Holmen <stho@oxx.no>
2018-07-09 13:55:16 +0200

    Add a build task that currently not working as expected caused by a bug in electron-webpack

--- template/src/main/index.ts
+++ template/src/main/index.ts
@@ -1,6 +1,19 @@
 import {app, BrowserWindow} from "electron";
+import path from "path";
+import {format as formatUrl} from "url";
+
+const isDevelopment = process.env.NODE_ENV;
 
 app.on("ready", () => {
     let window = new BrowserWindow();
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+    
+    if (isDevelopment) {
+        window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+    } else {
+        window.loadURL(formatUrl({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file",
+            slashes: true
+        }))
+    }
 });
